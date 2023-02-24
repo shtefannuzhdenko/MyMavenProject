6dc84c44bdc7ec9bc51d9eb7546a3ab1a6ba9239
shtefanuz <s-nuzhdenko@mail.ru>
2023-02-23 21:39:28 -0600

    The second push

--- src/test/java/technostudyB7/SeleniumProject/ValidateLocationIconFunctionality.java
+++ src/test/java/technostudyB7/SeleniumProject/ValidateLocationIconFunctionality.java
@@ -11,13 +11,14 @@ public class ValidateLocationIconFunctionality extends UtilityClass {
         /**
          * As a User I should be able to login by entering correct username and correct password
          * and selecting a location field. Then When I click on login button I should be navigated to HomePage
-         *
+         ****
          * 1- Once you are in Login page
          * 2- Enter valid username into the “Username” field
          * 3- Enter valid password in to the “Password” field
          * 4- Select one of the locations
          * 5- Click on “Login” button
          */
+
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
         WebElement logIn = driver.findElement(By.id("username"));
         logIn.sendKeys("Admin" + Keys.TAB + "Admin123" + Keys.ENTER);
